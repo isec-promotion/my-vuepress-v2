8f244deb5632f09a3f0392ca33f4d73f8916b1e2
Taiki Nakamura <tai-nakamura@isecj.jp>
2023-02-08 10:03:16 +0900

    test

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -32,9 +32,6 @@ export default defineUserConfig({
       appId: 'QDFAGFTOVQ',
       apiKey: 'daab5033f238b100d0af3bd40572f9e3',
       indexName: 'sample001',
-      searchParameters: {
-        // facetFilters: ["tags:v2"],
-      },
       locales: {
         '/': {
           placeholder: 'ドキュメント検索',
