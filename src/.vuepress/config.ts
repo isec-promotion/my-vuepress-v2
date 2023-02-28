68bec42a6aa8656ae5faeeb5d3f172e50021ee46
Taiki Nakamura <tai-nakamura@isecj.jp>
2023-02-28 13:54:01 +0900

    lang設定削除

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,11 +10,9 @@ const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   base: '/',
-  lang: 'ja-JP',
 
   locales: {
     '/': {
-      lang: 'ja-JP',
       title: 'オンライン取説',
       description: 'A docs demo for vuepress-theme-hope',
     },
